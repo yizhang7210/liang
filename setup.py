698eec7393bc6c08526fd28f67d337701f81f657
Yi Zhang <yi.zhang7210@gmail.com>
2021-11-07 20:57:29 -0800

    Bump to 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="python-liang",
-    version="0.0.2",
+    version="0.0.3",
     description="Incorporate non-functional requirements in function definitions",
     long_description=README,
     long_description_content_type="text/markdown",
